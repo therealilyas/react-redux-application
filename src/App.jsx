b79bdf98045fa0531e5bec29c2759f83b6a0e2bf
Your Name <ilyos.sultanov2020@gmail.com>
2023-06-19 16:14:48 +0500

    .

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Main, Login, Register } from "./components";
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
